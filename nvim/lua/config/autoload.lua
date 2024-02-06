e9369614a6be9719d4099273cee9362e83484e73
Luke Viens <lukeviens@gmail.com>
2024-02-06 14:35:17 -0800

    nvim config

---
+++ nvim/lua/config/autoload.lua
@@ -0,0 +1,14 @@
+vim.cmd([[
+  set number relativenumber
+  set tabstop=4
+  set shiftwidth=4
+  autocmd Filetype lua setlocal tabstop=2
+  autocmd Filetype lua setlocal shiftwidth=2
+
+]])
+
+
+vim.cmd([[
+  Neotree show
+]])
+
