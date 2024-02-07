1083842fef430d0cde4f1c0e73d1dec0b4d15d50
Luke Viens <luke@jiko.io>
2024-02-06 16:26:35 -0800

    update nvim and add wezterm

--- nvim/lua/config/autoload.lua
+++ nvim/lua/config/autoload.lua
@@ -1,14 +1,15 @@
+-- general vim configs
 vim.cmd([[
   set number relativenumber
   set tabstop=4
   set shiftwidth=4
   autocmd Filetype lua setlocal tabstop=2
   autocmd Filetype lua setlocal shiftwidth=2
-
 ]])
 
-
+-- neotree configs
 vim.cmd([[
   Neotree show
+	Neotree %:p:h
 ]])
 
