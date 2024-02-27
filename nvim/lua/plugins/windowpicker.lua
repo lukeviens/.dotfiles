48ba0baf0aa63d2a548b67cd79f83f67861cb9a0
Luke Viens <lukeviens@gmail.com>
2024-02-27 15:20:57 -0800

    edit nvim-window-picker

--- nvim/lua/plugins/windowpicker.lua
+++ nvim/lua/plugins/windowpicker.lua
@@ -1,9 +1,12 @@
 return {
-    's1n7ax/nvim-window-picker',
-    name = 'window-picker',
-    event = 'VeryLazy',
-    version = '2.*',
-    config = function()
-        require'window-picker'.setup()
-    end,
+	's1n7ax/nvim-window-picker',
+	name = 'window-picker',
+	event = 'VeryLazy',
+	version = '2.*',
+	config = function()
+		require'window-picker'.setup({
+			hint = 'floating-big-letter'
+		})
+
+	end,
 }
