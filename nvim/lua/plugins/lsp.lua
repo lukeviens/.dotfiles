6d4ed7c8a3323d44b06750450ecf26a7abd46d09
luke.viens <luke.viens@bytedance.com>
2024-05-01 13:53:37 -0700

    lsp binds

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,18 +17,18 @@ return {
 		local cmp = require('cmp')
 		local cmp_lsp = require("cmp_nvim_lsp")
 		local on_attach = function(client, bufnr)
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = bufnr, desc = "Go to Declaration" })
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, desc = "Go to Definition" })
+			-- Using Telescope for jumping to declaration, definition, and references
+			vim.keymap.set("n", "gD", require('telescope.builtin').lsp_type_definitions, { buffer = bufnr, desc = "Go to Type Definition" })
+			vim.keymap.set("n", "gd", require('telescope.builtin').lsp_definitions, { buffer = bufnr, desc = "Go to Definition" })
+			vim.keymap.set("n", "gi", require('telescope.builtin').lsp_implementations, { buffer = bufnr, desc = "Go to Implementation" })
+			vim.keymap.set("n", "gr", require('telescope.builtin').lsp_references, { buffer = bufnr, desc = "Symbol References" })
+
+			-- Retaining other LSP functionalities with original bindings
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr, desc = "LSP Hover" })
-			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { buffer = bufnr, desc = "Go to Implementation" })
 			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, { buffer = bufnr, desc = "Signature Help" })
-			-- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, desc = "Rename Symbol" })
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, { buffer = bufnr, desc = "Symbol References" })
-			-- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr, desc = "Code Action" })
 			vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { buffer = bufnr, desc = "Go to Next Diagnostic" })
-			vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = bufnr, desc = "Open Diagnostic Float" })
 			vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { buffer = bufnr, desc = "Go to Previous Diagnostic" })
-			-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { buffer = bufnr, desc = "Diagnostic to local list" })
+			vim.keymap.set("n", "gl", vim.diagnostic.open_float, { buffer = bufnr, desc = "Open Diagnostic Float" })
 		end
 		local capabilities = vim.tbl_deep_extend(
 			"force",
