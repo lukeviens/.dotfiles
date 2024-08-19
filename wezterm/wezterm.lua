40f83a47714e04694c1aa0d15afcc86a4d1b5793
Luke Viens <lukeviens@gmail.com>
2024-08-18 22:02:27 -0700

    wezterm updates

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -10,9 +10,26 @@ config.font = wezterm.font 'CaskaydiaCove NFM'
 --config.color_scheme = 'Ashes (dark) (terminal.sexy)'
 --config.color_scheme = 'Atelier Cave (base16)'
 --config.color_scheme = 'Chalk (dark) (terminal.sexy)'
-
 config.color_scheme = 'Darktooth (base16)'
 
-config.window_background_opacity = 1
+config.window_decorations = "INTEGRATED_BUTTONS | RESIZE | MACOS_FORCE_ENABLE_SHADOW"
+config.integrated_title_button_style = "Windows"
+
+config.window_background_opacity = .995
+
+config.window_frame = {
+  border_left_width = '0.25cell',
+  border_right_width = '0.25cell',
+  border_bottom_height = '0.13cell',
+  border_top_height = '0.13cell',
+  border_left_color = '#3f3a39',
+  border_right_color = '#3f3a39',
+  border_bottom_color = '#3f3a39',
+  border_top_color = '#3f3a39',
+}
+
+config.use_fancy_tab_bar = true
+
+config.enable_tab_bar = false
 
 return config
