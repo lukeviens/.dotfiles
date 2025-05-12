22586015446172c908e17303040afd6f0fdf0337
luke viens <lukeviens@gmail.com>
2025-05-12 10:27:16 -0700

    fix animate lol

--- nvim/lua/config/autoload.lua
+++ nvim/lua/config/autoload.lua
@@ -7,79 +7,85 @@
 --  - different entity types, e.g. hole consumes, tree stops, etc. 
 --  - then, better randomization of scene and events
 
-local first_buffer = vim.api.nvim_get_current_buf()
-
-local starts = {"🌳"}
-local ends   = {"🕳️"}
-local scene = {" ", " ", " ", " ", " ", " ", " "}
-scene[1] = starts[math.random(#starts)]
-scene[#scene] = ends[math.random(#ends)]
-
-local things = {"🧍‍♂️", "🧍‍♀️"}
-local thing = things[math.random(#things)]
-local thing_pos = 2
-local thing_dir = 1
-local timer = vim.loop.new_timer()
-
-scene[thing_pos] = thing
-
-local function render_scene()
-  local scene_str = table.concat(scene)
-  if vim.api.nvim_buf_is_valid(first_buffer) then
-    vim.api.nvim_buf_set_name(first_buffer, scene_str)
-  end
-end
-render_scene()
-
-local function is_action(pct)
-  return math.random(100) <= pct
+local function animate_buffer()
+  return vim.fn.argc() == 0
 end
 
-local function run_scene()
-  -- decide action 
-  if thing_pos == #scene or thing_pos == 2 then
-    if not is_action(10) then return end
-  else
-    if not is_action(25) then return end
+if animate_buffer() then
+  local first_buffer = vim.api.nvim_get_current_buf()
+
+  local starts = {"🌳"}
+  local ends   = {"🕳️"}
+  local scene = {" ", " ", " ", " ", " ", " ", " "}
+  scene[1] = starts[math.random(#starts)]
+  scene[#scene] = ends[math.random(#ends)]
+
+  local things = {"🧍‍♂️", "🧍‍♀️"}
+  local thing = things[math.random(#things)]
+  local thing_pos = 2
+  local thing_dir = 1
+  local timer = vim.loop.new_timer()
+
+  scene[thing_pos] = thing
+
+  local function render_scene()
+    local scene_str = table.concat(scene)
+    if vim.api.nvim_buf_is_valid(first_buffer) then
+      vim.api.nvim_buf_set_name(first_buffer, scene_str)
+    end
   end
-
-  -- update the direction
-  if thing_pos == #scene then
-    thing = things[math.random(#things)]
-    thing_dir = -1
-  elseif thing_pos == 2 then
-    thing_dir = 1
-  end
-
-  -- update the position 
-  thing_pos = thing_pos + thing_dir
-
-  -- in the hole
-  if thing_pos == #scene then
-    local w = vim.fn.strdisplaywidth(thing)
-    scene[thing_pos-1] = string.rep(" ", w)
-  -- leaving the hole
-  elseif thing_pos == #scene - 1 and thing_dir == -1 then
-    scene[thing_pos] = thing
-  -- out of the hole
-  else
-    scene[thing_pos - thing_dir] = " "
-    scene[thing_pos] = thing
-  end
-
-  -- render the scene  
   render_scene()
+
+  local function is_action(pct)
+    return math.random(100) <= pct
+  end
+
+  local function run_scene()
+    -- decide action 
+    if thing_pos == #scene or thing_pos == 2 then
+      if not is_action(10) then return end
+    else
+      if not is_action(25) then return end
+    end
+
+    -- update the direction
+    if thing_pos == #scene then
+      thing = things[math.random(#things)]
+      thing_dir = -1
+    elseif thing_pos == 2 then
+      thing_dir = 1
+    end
+
+    -- update the position 
+    thing_pos = thing_pos + thing_dir
+
+    -- in the hole
+    if thing_pos == #scene then
+      local w = vim.fn.strdisplaywidth(thing)
+      scene[thing_pos-1] = string.rep(" ", w)
+    -- leaving the hole
+    elseif thing_pos == #scene - 1 and thing_dir == -1 then
+      scene[thing_pos] = thing
+    -- out of the hole
+    else
+      scene[thing_pos - thing_dir] = " "
+      scene[thing_pos] = thing
+    end
+
+    -- render the scene  
+    render_scene()
+  end
+
+  timer:start(0, 200, vim.schedule_wrap(run_scene))
+
+  vim.api.nvim_create_autocmd("BufWipeout", {
+    buffer = first_buffer,
+    callback = function()
+      timer:stop()
+      timer:close()
+    end,
+  })
 end
-
-timer:start(0, 200, vim.schedule_wrap(run_scene))
-
-vim.api.nvim_create_autocmd("BufWipeout", {
-  buffer = first_buffer,
-  callback = function()
-    timer:stop()
-    timer:close()
-  end,
-})
 
 -- tabs/spaces 
 vim.cmd([[
